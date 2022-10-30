1d4748852abec6a86b6345b756210396b3cbba2b
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-30 22:11:14 +0700

    change variable to ES6

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,33 +1,33 @@
 var main = function () {
-  var searchElement = document.querySelector('#searchElement');
-  var buttonSearchElement = document.querySelector('#searchButtonElement');
-  var clubListElement = document.querySelector('#clubList');
+  const searchElement = document.querySelector('#searchElement');
+  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const clubListElement = document.querySelector('#clubList');
 
-  var onButtonSearchClicked = function () {
-    var dataSource = new DataSource(renderResult, fallbackResult);
+  const onButtonSearchClicked = function () {
+    let dataSource = new DataSource(renderResult, fallbackResult);
     dataSource.searchClub(searchElement.value);
   };
 
-  var renderResult = function (results) {
+  const renderResult = function (results) {
     clubListElement.innerHTML = '';
     results.forEach(function (club) {
-      var name = club.name;
-      var fanArt = club.fanArt;
-      var description = club.description;
+      let name = club.name;
+      let fanArt = club.fanArt;
+      let description = club.description;
 
-      var clubElement = document.createElement('div');
+      const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
 
       clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-          '<div class="club-info">\n' +
-          '<h2>' + name + '</h2>\n' +
-          '<p>' + description + '</p>' +
-          '</div>';
+        '<div class="club-info">\n' +
+        '<h2>' + name + '</h2>\n' +
+        '<p>' + description + '</p>' +
+        '</div>';
       clubListElement.appendChild(clubElement);
     });
   };
 
-  var fallbackResult = function (message) {
+  const fallbackResult = function (message) {
     clubListElement.innerHTML = '';
     clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
   };
