236a9f8bbb160540635553adf4d0ef087452622c
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-30 22:17:53 +0700

    change variable to ES6

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,7 +1,7 @@
 var main = function () {
-  const searchElement = document.querySelector('#searchElement');
-  const buttonSearchElement = document.querySelector('#searchButtonElement');
-  const clubListElement = document.querySelector('#clubList');
+  const searchElement = document.querySelector("#searchElement");
+  const buttonSearchElement = document.querySelector("#searchButtonElement");
+  const clubListElement = document.querySelector("#clubList");
 
   const onButtonSearchClicked = function () {
     const dataSource = new DataSource(renderResult, fallbackResult);
@@ -9,28 +9,30 @@ var main = function () {
   };
 
   const renderResult = function (results) {
-    clubListElement.innerHTML = '';
+    clubListElement.innerHTML = "";
     results.forEach(function (club) {
       const name = club.name;
       const fanArt = club.fanArt;
       const description = club.description;
 
-      const clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
+      const clubElement = document.createElement("div");
+      clubElement.setAttribute("class", "club");
 
-      clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-        '<div class="club-info">\n' +
-        '<h2>' + name + '</h2>\n' +
-        '<p>' + description + '</p>' +
-        '</div>';
+      clubElement.innerHTML = `
+                              <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+                              <div class="club-info">
+                                <h2>${name}</h2>
+                                <p>${description}</p>
+                              </div>
+                            `;
       clubListElement.appendChild(clubElement);
     });
   };
 
   const fallbackResult = function (message) {
-    clubListElement.innerHTML = '';
-    clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
+    clubListElement.innerHTML = "";
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
-  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
